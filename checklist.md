65e22910187b1c446e39bdce0571dba20c88892b
cathywidén <katalinwiden@gmail.com>
2022-11-23 19:19:03 +0100

    Buggy for new user, otherwise OK.
    Known issues
    
    "Welcome, null" on new user login, not the name I entered
    
    new user isn't persistent on page refresh
    
    Need to fix switch or else.. f for new user reg if passwords don't match

--- checklist.md
+++ checklist.md
@@ -1,36 +1,38 @@
 generic
 
-[x] basic html/css OBS without input fields (those are dynamic)
+[x] basic html/css OBS without input fields as those are dynamic content
 [x] header Welcome!
+[x] footer
 [x] set up predefined user/pass combos
-[x] user/pass janne/test
+[x] include janne/test
 [x] document all preset users in readme.md
 
 
 fix three page layouts
 
-[ ] (1) default with user/pass input fields
-[ ] (2) logged in: welcome [user] message 
-[ ] logout option
-[ ] (3) registration page
+[x] (1) default with user/pass input fields
+[x] (2) logged in: welcome [user] message 
+[x] logout option
+[x] (3) registration page
 
 
 dynamic content via js
 
-[ ] if not logged in, show login page
-[ ] if logged in, welcome [user] + logout option
-[ ] error message if wrong password entered
-[ ] logged in status saved in localStorage
+[x] if not logged in, show login page
+[x] if logged in, welcome [user] + logout option
+[x] error message if wrong password entered
+[x] logged in status saved in localStorage
 
 
 new user registration
 
-[ ] error message if no password is entered
+[x] error message if no password is entered
 [ ] new user logged in status saved in localStorage
 
 
 other
 
-[ ] make it pretty
+[?] make it pretty
 [ ] clean up code
 [ ] upload to Github Pages
+[ ] set repo to public
--- script.js
+++ script.js
@@ -1,53 +1,133 @@
-// PRESET USER/PASS
-let loginData = [
-    {userName:"janne", password: "test"},
-    {userName:"cathy", password: "test"},
-    {userName:"test", password: "test"},
+// Preset user/pass combos
+
+let presetUsers = [
+    {
+userName:"janne", 
+password: "test"},
+    {
+userName:"cathy", 
+password: "test"},
+    {	
+userName:"defineinsane", 
+password: "test"},
 ];
 
-// LAYOUTS
-/* Set up the three different layouts with JS (let):
-
-
-   (1) default view with user/pass input fields
-
-
-
-   (2) view when logged in: Welcome [user] + logout option
-   On logout: clear [user] data from localStorage!
-
-
-
-   (3) registration view 
+/*
+   Set up the three different layouts with JS (let):
+   (1) defaultPage with user/pass input fields
+   (2) loggedInPage: Welcome [user] + logout button
+   (3) regPage 
 */
 
+let defaultPage =`<p>Please log in!</p>
+<label for="username">Username</label>
+<input type="text" id="inputUsername"> <br><br>
+<label for="password">Password</label>
+<input type="text" id="inputPassword"> <br><br>
+<button id="loginBtn">Login</button> 
+<button id="regBtn"> Register </button> <br><br>
+<p id="message"></p>`;
+
+let loggedInPage = `<P>Welcome ${localStorage.getItem('usernameKey')}! You have logged in.</p>
+<button id=logoutBtn>Log out</button>`;
+
+let regPage =`<label for="userName">Register desired username</label>
+<input type="text" id="newUser"> <br><br>
+<label for="password">Enter new password</label>
+<input type="text" id="newPassword"> <br><br>
+<label for="password">Confirm new password</label>
+<input type="text" id="newPasswordConfirm"> <br><br>
+<button id="confirmBtn">confirm</button> <br>
+<p id="regMessage"></p>`
 
 
+// Check if there’s anyone logged in. If so, redirect to loggedInPage. Else, show defaultPage
+
+if (localStorage.usernameKey) {
+
+    content.innerHTML = loggedInPage; 
+
+    let logoutBtn = document.getElementById("logoutBtn");
+
+        logoutBtn.addEventListener("click", function (){
+        	localStorage.removeItem("usernameKey");
+        	content.innerHTML= defaultPage;
+        	location.reload(); 
+    })
+}
+
+else {
+       content.innerHTML= defaultPage;
+    
+    let loginBtn = document.getElementById("loginBtn");
+
+        loginBtn.addEventListener("click", function () { 
+            let inputUsername = document.getElementById("inputUsername");
+            let inputPassword = document.getElementById("inputPassword");
+            
+        
+            inputPassword.style.backgroundColor = 'green';
+              
+
+     // Check if user exists: compare both entered username and pass with preset list in [presetUsers]
+
+        for (data in presetUsers){ 
+
+            if (inputUsername.value === presetUsers[data].userName && inputPassword.value === presetUsers[data].password) {
+          
+                localStorage.usernameKey = inputUsername.value;
+                content.innerHTML = loggedInPage
+                location.reload();             
+            }
+            
+            else {
+                let message = document.getElementById("message");
+                message.innerHTML="Username or password is wrong. Please try again.";
+            }   
+        }    
+    });
+ 
+// Register new user
+
+    let regBtn=document.getElementById("regBtn");
+    regBtn.addEventListener("click", function () {
+        content.innerHTML= regPage;
+
+         // Confirm registration
+
+        let confirmBtn = document.getElementById("confirmBtn");
+
+        confirmBtn.addEventListener("click", function () {
+
+            let newUser=document.getElementById("newUser");
+                if (newUser.value && newPassword.value && (newPassword.value === newPasswordConfirm.value)) {
+
+                    // Use .push to add user to [presetUsers]
+                 
+                    presetUsers.push({userName: newUser.value, password: newPassword.value});
+
+                    // Add newly reg. user to localStorage
+
+                    localStorage.setItem(("presetUsers"), JSON.stringify(presetUsers));
+                    let getPresetUsers = JSON.parse(localStorage.getItem("presetUsers"));  
+
+                    content.innerHTML=loggedInPage;
+
+                    let logoutBtn = document.getElementById("logoutBtn");
+                    logoutBtn.addEventListener("click", function (){
+                        localStorage.removeItem("presetUsers");
+                        content.innerHTML= defaultPage;
+                        location.reload(); 
+                    })
+                }
+                else {
+                    console.log("Please fill in username and password.");
+                    let regMessage = document.getElementById("regMessage");
+                    regMessage.innerHTML=" Please specify both a valid username and a password. Make sure the passwords match.";
+                }    
+        })
+    })
+    
+}
 
 
-
-// LOGIN
-/* Check localStorage if there's any active login already:
-
-   If so, redirect to logged-in view
-
-   } else {
-
-   redirect to default view: 
-   Check input data against [loginData] with a loop
-   If login matches, redirect to logged-in view, welcome [user], and add [user] to localStorage 
-
-   } else {
-   Wrong username or password, try again
-*/
-
-
-// REGISTER NEW USER
-/* 
-   Use .push to add user to pre-set [loginData]
-   add newly reg. user to localStorage
-   Display logged in view and logout option
-   On logout, remove from localStorage
-
-   Do it with an if statement --> empty password: error message
-*/
