e36f37aeac09fa7d4811058405be3add1f5a5116
cathywidén <katalinwiden@gmail.com>
2022-11-26 20:16:20 +0100

    Minor bugs left
    To solve
    
    -- fix login directly from conflictPage

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ if (localStorage.getItem("accounts") === null) {
 
 // defaultPage with user/pass input fields
 let defaultPage = `<p>Please log in!</p>
-<label for="username">username</label>
+<label for="username">Username</label>
 <input type="text" id="inputUsername"><br><br>
 <label for="password">Password</label>
 <input type="text" id="inputPassword"><br><br>
@@ -52,11 +52,8 @@ let regPage = `<label for="username">Register username</label>
 <p id="regMessage"></p>`
 
 
-
-
 function logIn(page) {
     console.log("Logged in");
-
     content.innerHTML = page;
 
     let loginBtn = document.getElementById("loginBtn");
@@ -68,7 +65,6 @@ function logIn(page) {
 
         // Check if user exists
         for (data in accounts) {
-            //console.log(accounts[data].username);
 
             if (inputUsername.value === accounts[data].username && inputPassword.value === accounts[data].password) {
                 localStorage.loggedInUser = inputUsername.value;
@@ -84,8 +80,8 @@ function logIn(page) {
         }
     });
 }
-// Check if there’s anyone logged in. If so, redirect to loggedInPage. Else, show defaultPage
 
+// Check if there’s anyone logged in. If so, redirect to loggedInPage. Else, show defaultPage
 if (localStorage.loggedInUser) {
     content.innerHTML = loggedInPage;
 
@@ -99,104 +95,94 @@ if (localStorage.loggedInUser) {
 
 else {
     logIn(defaultPage);
-    };
+};
 
-    // Register new user
-    let regBtn = document.getElementById("regBtn");
+// Register new user
+let regBtn = document.getElementById("regBtn");
 
 
-    regBtn.addEventListener("click", function () {
+regBtn.addEventListener("click", function () {
 
-        content.innerHTML = regPage;
-        document.getElementById("headertext").innerHTML = "Register here";
+    content.innerHTML = regPage;
+    document.getElementById("headertext").innerHTML = "Register here";
 
-        // Confirm registration
-        let confirmBtn = document.getElementById("confirmBtn");
-        let newUser = document.getElementById("newUser");
-        let newPassword = document.getElementById("newPassword");
-        let newPasswordConfirm = document.getElementById("newPasswordConfirm");
-        let regMessage = document.getElementById("regMessage");
+    // Confirm registration
+    let confirmBtn = document.getElementById("confirmBtn");
+    let newUser = document.getElementById("newUser");
+    let newPassword = document.getElementById("newPassword");
+    let newPasswordConfirm = document.getElementById("newPasswordConfirm");
+    let regMessage = document.getElementById("regMessage");
 
-        newUser.addEventListener("input", () => {
-            let accounts = JSON.parse(localStorage.getItem("accounts"));
-            for (data in accounts) {
-                console.log(newUser.value);
+    newUser.addEventListener("input", () => {
+        let accounts = JSON.parse(localStorage.getItem("accounts"));
+        for (data in accounts) {
+            console.log(newUser.value);
 
-                // Disallow registration with existing credentials
-                if (newUser.value === accounts[data].username) {
-                    regMessage.innerHTML = `This username already exists. Pick another.
+            // Disallow registration with existing credentials
+            if (newUser.value === accounts[data].username) {
+                regMessage.innerHTML = `This username already exists. Pick another.
                      <br><p> If you <strong>are</strong> "${newUser.value}", you can log in here:
                      <br><innerHTML = "${conflictPage}</a>`;
-                    newUser.style.backgroundColor = "yellow";
+                newUser.style.backgroundColor = "yellow";
 
-                    let confirmBtn = document.querySelector("confirmBtn");
-                    //confirmBtn.setAttribute("disabled", true);
-                    //confirmBtn.setAttribute("disabled", "");
-                    //confirmBtn.setAttribute("disabled", null);
-                    confirmBtn.removeAttribute("confirmBtn");
+                let confirmBtn = document.querySelector("confirmBtn");
+                confirmBtn.removeAttribute("confirmBtn");
 
-                    //conflictPage
-                    //if (content.innerHTML = conflictPage) {
-                        console.log("test");
-                        logIn(conflictPage);
-                        // run login()
-                        // Re-write login() so it can be run here also
-                    }
-                else {
-                    newUser.style.backgroundColor = "white";
-                    regMessage.innerHTML = "";
+                //conflictPage
+                //if (content.innerHTML = conflictPage) {
 
-                    //button.disabled = false;
-                    //.push
-                }
+                logIn(defaultPage);
+                // run login()
+
             }
-        });
-
-        confirmBtn.addEventListener("click", () => {
-            let accounts = JSON.parse(localStorage.getItem("accounts"));
-            // new username correctly given, and passwords match
-            if ((newUser.value !== accounts[data].username) && newPassword.value && (newPassword.value === newPasswordConfirm.value)) {
-
-                content.innerHTML = loggedInPage;
-                
-                accounts.push({ username: newUser.value, password: newPassword.value });
-                localStorage.setItem("loggedInUser", newUser.value);
-                localStorage.setItem(("accounts"), JSON.stringify(accounts));
-
-                //console.log(newUser.value);
-                //console.log(accounts);
-
-                location.reload();
-
-
-                let logoutBtn = document.getElementById("logoutBtn");
-                logoutBtn.addEventListener("click", function () {
-                    localStorage.removeItem("loggedInUser");
-                    content.innerHTML = defaultPage;
-                    location.reload();
-                })
-            }
-
-            // passwords don't match
-            else if (newUser.value && newPassword.value && newPasswordConfirm.value && (newPassword.value !== newPasswordConfirm.value)) {
-
-                regMessage.innerHTML = "Your passwords do not match. Please try again.";
-                newPasswordConfirm.style.backgroundColor = "yellow";
-            }
-
-            // user didn't fill in some field(s)
             else {
-                console.log("Please fill in username and password.");
-
-                regMessage.innerHTML = "Please specify both a valid username and a password.";
-                newPasswordConfirm.style.backgroundColor = "white";
+                newUser.style.backgroundColor = "white";
+                regMessage.innerHTML = "";
+                //button.disabled = false;
             }
+        }
+    });
+
+    confirmBtn.addEventListener("click", () => {
+        let accounts = JSON.parse(localStorage.getItem("accounts"));
+
+        // new username correctly given, and passwords match
+        if ((newUser.value !== accounts[data].username) && newPassword.value && (newPassword.value === newPasswordConfirm.value)) {
+
+            content.innerHTML = loggedInPage;
+
+            accounts.push({ username: newUser.value, password: newPassword.value });
+            localStorage.setItem("loggedInUser", newUser.value);
+            localStorage.setItem(("accounts"), JSON.stringify(accounts));
+
+            //console.log(newUser.value);
+            //console.log(accounts);
+
+            location.reload();
 
 
+            let logoutBtn = document.getElementById("logoutBtn");
+            logoutBtn.addEventListener("click", function () {
+                localStorage.removeItem("loggedInUser");
+                content.innerHTML = defaultPage;
+                location.reload();
+            })
         }
 
+        // passwords don't match
+        else if (newUser.value && newPassword.value && newPasswordConfirm.value && (newPassword.value !== newPasswordConfirm.value)) {
 
+            regMessage.innerHTML = "Your passwords do not match. Please try again.";
+            newPasswordConfirm.style.backgroundColor = "yellow";
+        }
 
-        )
+        // user didn't fill in some field(s)
+        else {
+            console.log("Please fill in username and password.");
 
-    })
+            regMessage.innerHTML = "Please specify both a valid username and a password.";
+            newPasswordConfirm.style.backgroundColor = "white";
+        }
+    }
+    )
+})
